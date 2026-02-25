f1098be8631a8b347699da53efd9e263061c893d
qinjiapeng <759848795@qq.com>
2026-02-25 19:38:35 +0800

    feat: 设置 antd 语言为中文（zhCN）

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { StyleProvider } from "@ant-design/cssinjs";
 import { ConfigProvider, theme } from "antd";
+import zhCN from "antd/locale/zh_CN";
 import App from "./App";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ConfigProvider
+      locale={zhCN}
       theme={{
         algorithm: theme.darkAlgorithm,
         token: {
